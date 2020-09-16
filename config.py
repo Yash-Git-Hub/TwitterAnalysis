debd264c4a6a7bd85c0a5382d859ec48dad0ca43
Yash-Git-Hub <newtoneinsitenfarady@gmail.com>
2020-09-16 22:14:19 +0530

    commit

---
+++ config.py
@@ -0,0 +1,5 @@
+#gitignore
+consumerKey = "CMxjz8lBpTj79uoDBB4ynXAUP"
+consumerSecret = "C1nhpvvYV0f5Vzdp99JO5Jpen89uQotLDCoIrKqZBoPWrrMk6w"
+accessToken = "1275165485206835202-6PRnhAlF6D4e8PwoSM1Hw7BVMIACpk"
+accessTokenSecret = "WJcsD5KD6aLmY7Guso89eR8Oy4ag8gxAaAKo3PguWZX2S"
